1883abf359a1c78d698e5c5a792c29640a4a88e3
jéssica senger <jessicasbogoni1@gmail.com>
2019-05-23 11:51:25 -0300

    Define tests of the first test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,17 +26,21 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it('have URL', function() {
+            
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('have name', function() {
+
+        });
     });
 
 
