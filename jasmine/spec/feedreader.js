241baceaf070654cdc5bd5c0b7cd979a69454f63
jéssica senger <jessicasbogoni1@gmail.com>
2019-05-26 12:52:15 -0300

    Update comments on tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,11 +1,18 @@
 $(function() {
     describe('RSS Feeds', function() {
 
+        /* Make sure that the allFeeds variable has been
+         * defined and that it is not empty
+         */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
+        /* Loop through each feed in the allFeeds
+         * object and ensures it has a URL defined
+         * and that the URL is not empty.
+         */
         it('have URL', function() {
             for(feed of allFeeds) {
                 expect(feed.url).toBeDefined();
@@ -13,6 +20,10 @@ $(function() {
             }
         });
 
+        /* Loop through each feed in the allFeeds
+         * object and ensures it has a name defined
+         * and that the name is not empty.
+         */
         it('have name', function() {
             for(feed of allFeeds) {
                 expect(feed.name).toBeDefined();
@@ -23,10 +34,15 @@ $(function() {
 
     describe('The menu', function() {
 
+        /* Ensure the menu element is hidden by default.
+         */
         it('is hidden by default', function() {
             expect($('body')[0]).toHaveClass('menu-hidden');
         });
 
+         /* Ensure the menu changes visibility
+          * when the menu icon is clicked.
+          */
          it('changes visibility when icon is clicked', function() {
             $('.menu-icon-link').click();
             expect($('body')[0]).not.toHaveClass('menu-hidden');
@@ -38,6 +54,10 @@ $(function() {
 
     describe('Initial Entries', function() {
 
+        /* Ensure that when the loadFeed function is called and
+         * completes its work, there is at least a single
+         * .entry element within the .feed container.
+         */
         beforeEach(function(done) {
             loadFeed(0, done);
         });
@@ -51,6 +71,10 @@ $(function() {
     });
 
     describe('New Feed Selection', function() {
+
+        /* Ensures that when a new feed is loaded by the loadFeed
+         * function that the content actually changes.
+         */
         let beforeFeed, afterFeed;
 
         beforeEach(function(done) {
